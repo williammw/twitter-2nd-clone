aef80a80e2da7344bafb31b58042dc93ba224f06
wwwwwmw <william.manwai@gmail.com>
2022-06-05 00:39:14 +0800

    object fecth from ssanity

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Tweet } from '../typings'
+import TimeAgo from 'timeago-react'
 
 interface Props { 
   tweet:Tweet
@@ -8,8 +9,32 @@ interface Props {
 
 function Tweet({tweet}:Props) {
   return (
-    <div>
-      
+    <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImg} alt='' />
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+              <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/g,'').toLowerCase()}
+              </p>
+
+
+            <TimeAgo 
+            className='text-sm text-gray-500'
+            date={tweet._createdAt} 
+            />
+          </div>
+
+          <p className='pt-1'>{tweet.text}</p>
+
+          {tweet.image && <img src={tweet.image} alt="" 
+          className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm" 
+          />}
+        </div>
+      </div>
     </div>
   )
 }
